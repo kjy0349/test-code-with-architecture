9301372db980c880e778d2e82baab5f84c904703
kjy0349 <kjy0349@naver.com>
2024-04-12 08:56:08 +0900

    refactor: 각 서비스 메소드 명확한 이름으로 수정

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findByIdAndStatus() {
+    }
+
+    @Test
+    void findByEmailAndStatus() {
+    }
+}
