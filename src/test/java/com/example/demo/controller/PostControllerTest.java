532e939512d52857984c3345ee583d09f71b1ebb
kjy0349 <kjy0349@naver.com>
2024-04-16 12:29:56 +0900

    test: Post 관련 Controller 테스트 추가

--- src/test/java/com/example/demo/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/controller/PostControllerTest.java
@@ -1,20 +1,76 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostUpdateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.PostUpdate;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD),
+        @Sql(value = "/sql/post-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+})
 class PostControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
     @Test
-    void getPostById() {
+    void ID로_게시글을_불러올_수_있다() throws Exception{
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/1"))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.content").value("내용 입니다."));
     }
 
     @Test
-    void updatePost() {
+    void 사용자는_존재하지_않는_게시글을_조회할_경우_에러가_난다() throws Exception{
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/112312312"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Posts에서 ID 112312312를 찾을 수 없습니다."));
     }
 
     @Test
-    void toResponse() {
+    void 사용자는_게시글을_수정할_수_있다() throws Exception{
+        // given
+        PostUpdateDto postUpdateDto = PostUpdateDto.builder()
+                .content("modifiedContent")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(put("/api/posts/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postUpdateDto)))
+                .andExpect(jsonPath("$.content").value("modifiedContent"))
+                .andExpect(jsonPath("$.id").value(1));
     }
 }
--- src/test/java/com/example/demo/controller/PostCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.demo.controller;
+
+import com.example.demo.model.dto.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD),
+        @Sql(value = "/sql/user-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+})
 class PostCreateControllerTest {
-  
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void 사용자는_게시글을_등록할_수_있다 () throws Exception {
+        // given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .content("postCreateTestContent")
+                .writerId(1)
+                .build();
+        // when
+        // then
+        mockMvc.perform(post("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(jsonPath("$.writer.id").value(1))
+                .andExpect(jsonPath("$.content").value("postCreateTestContent"));
+    }
 }
